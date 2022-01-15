d6e6afcbcaa284d7ec4948737800d964b031b477
RAMONA-GEORGIANA CHITICARIU <ramona.chiticariu@stud.ubbcluj.ro>
2022-01-15 21:39:51 +0200

    Add project files.

---
+++ Chiticariu_Ramona_Lab12/Chiticariu_Ramona_Lab12/Data/ShoppingListDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Chiticariu_Ramona_Lab12.Models;
+
+namespace Chiticariu_Ramona_Lab12.Data
+{
+    class ShoppingListDatabase
+    {
+        IRestService restService;
+        public ShoppingListDatabase(IRestService service)
+        {
+            restService = service;
+        }
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return restService.RefreshDataAsync();
+        }
+
+        public Task SaveShopListAsync(ShopList item, bool isNewItem = true)
+        {
+            return restService.SaveShopListAsync(item, isNewItem);
+        }
+        public Task DeleteShopListAsync(ShopList item)
+        {
+            return restService.DeleteShopListAsync(item.ID);
+        }
+
+    }
+}
+  
